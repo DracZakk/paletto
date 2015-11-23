7ec8e06ad772724db4ecca9d05214633fbc76e93
Nadir Trabelsi <nadir1310@gmail.com>
2015-11-23 22:22:16 +0100

    commit du test de l'histoire 3

--- test/test.js
+++ test/test.js
@@ -18,3 +18,12 @@ PalettoTestCase.prototype.test_secondStory = function () {
     assertTrue(x.check_player() === 1);
     assertTrue(x.check_corner("Yellow") === true);
 };
+
+PalettoTestCase.prototype.test_thirdStory = function () {
+    var column = "A", line = 6;
+    assertEquals(x.get_int_column(column), 0);
+    assertTrue(x.check_player() === 1);
+    assertEquals(x.remove_piece(line, column, 1), true);
+    assertEquals(x.get_nb_board_pieces(), 35);
+    assertEquals(x.get_nb_player_pieces(1), 1);
+};
